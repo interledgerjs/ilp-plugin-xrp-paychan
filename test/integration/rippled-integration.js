87c2fcf51ccf3c923efdeb32f65c94181c6631c7
Dennis Appelt <dappelt@ripple.com>
2018-03-06 19:44:49 +0000

    test: update tests for ilp-plugin-btp

--- test/integration/rippled-integration.js
+++ test/integration/rippled-integration.js
@@ -6,13 +6,12 @@ chai.use(chaiAsPromised)
 const assert = chai.assert
 const expect = chai.expect
 
+const ilpPacket = require('ilp-packet')
 const crypto = require('crypto')
-const uuid = require('uuid')
-const base64url = require('base64url')
 const BigNumber = require('bignumber.js')
 const PluginRipple = require('../../index.js')
 const RippleAPI = require('ripple-lib').RippleAPI
-const Store = require('ilp-plugin-payment-channel-framework/test/helpers/objStore')
+const Store = require('ilp-store-memory')
 const {payTo, ledgerAccept} = require('./utils')
 const { sleep, dropsToXrp } = require('../../src/lib/constants')
 
@@ -23,7 +22,7 @@ const COMMON_OPTS = {
   token: 'shared_secret',
   rippledServer: SERVER_URL,
   maxUnsecured: '5000',
-  maxAmount: '100000',
+  channelAmount: 100000,
   fundThreshold: '0.9'
 }
 
@@ -88,42 +87,26 @@ describe('plugin integration', function () {
   beforeEach(async function () {
     await setup.call(this, SERVER_URL)
 
-    const sharedSecret = 'shh its a secret'
+    const sharedSecret = 'secret'
     const serverHost = 'localhost'
     const serverPort = 3000
 
     this.plugin = new PluginRipple(Object.assign({}, COMMON_OPTS, {
-      // This plugin's ripple address and secret.
-      // Get testnet credentials at https://ripple.com/build/xrp-test-net/
       address: this.newWallet.address,
       secret: this.newWallet.secret,
-
-      // The peer you want to start a payment channel with
       peerAddress: this.peer.address,
-
-      // Our peer acts as BTP server. This is the address he is listening on
       server: `btp+ws://:${sharedSecret}@${serverHost}:${serverPort}`,
-
-      // Other options. For details see:
-      // https://github.com/interledger/rfcs/blob/master/0004-ledger-plugin-interface/0004-ledger-plugin-interface.md#class-pluginoptions
       _store: new Store()
     }))
     this.peerPlugin = new PluginRipple(Object.assign({}, COMMON_OPTS, {
-      // This plugin's ripple address and secret.
-      // Get testnet credentials at https://ripple.com/build/xrp-test-net/
       address: this.peer.address,
       secret: this.peer.secret,
-
-      // The peer you want to start a payment channel with
       peerAddress: this.newWallet.address,
-
-      // Other options. For details see:
-      // https://github.com/interledger/rfcs/blob/master/0004-ledger-plugin-interface/0004-ledger-plugin-interface.md#class-pluginoptions
       _store: new Store(),
       listener: {
-        port: serverPort
+        port: serverPort,
+        secret: sharedSecret
       },
-      incomingSecret: sharedSecret,
       prefix: 'g.xrp.mypaychan.',
       info: {
         prefix: 'g.xrp.mypaychan.',
@@ -133,7 +116,8 @@ describe('plugin integration', function () {
       }
     }))
 
-    this.pluginState = this.plugin._paychanContext.state
+    // this.plugin.registerDataHandler(() => {})
+    // this.peerPlugin.registerDataHandler(() => {})
 
     // automatically accept the ledger when api.submit() is called
     const autoAcceptLedger = (api) => {
@@ -145,9 +129,8 @@ describe('plugin integration', function () {
         })
       }
     }
-
-    autoAcceptLedger(this.plugin._paychanContext.state.api)
-    autoAcceptLedger(this.peerPlugin._paychanContext.state.api)
+    autoAcceptLedger(this.plugin._api)
+    autoAcceptLedger(this.peerPlugin._api)
   })
   afterEach(teardown)
 
@@ -155,22 +138,21 @@ describe('plugin integration', function () {
     it('is eventually fulfilled', async function () {
       const connectPromise = Promise.all([this.peerPlugin.connect(),
         this.plugin.connect()])
-      await sleep(10)
       return expect(connectPromise).to.be.eventually.fulfilled
     })
 
     it('creates an outgoing paychan', async function () {
       await connectPlugins(this.peerPlugin, this.plugin)
 
-      const paychanid = await this.pluginState.outgoingChannel.getMax()
-      const chan = await this.api.getPaymentChannel(paychanid.data)
+      const paychanid = this.plugin._outgoingChannel
+      const chan = await this.api.getPaymentChannel(paychanid)
 
       assert.strictEqual(chan.account, this.newWallet.address)
-      assert.strictEqual(chan.amount, dropsToXrp(COMMON_OPTS.maxAmount))
+      assert.strictEqual(chan.amount, dropsToXrp(COMMON_OPTS.channelAmount))
       assert.strictEqual(chan.balance, '0')
       assert.strictEqual(chan.destination, this.peer.address)
       assert.strictEqual(chan.settleDelay, COMMON_OPTS.settleDelay)
-      const expectedPubKey = 'ED' + Buffer.from(this.pluginState.keyPair.publicKey)
+      const expectedPubKey = 'ED' + Buffer.from(this.plugin._keyPair.publicKey)
         .toString('hex').toUpperCase()
       assert.strictEqual(chan.publicKey, expectedPubKey)
     })
@@ -178,17 +160,16 @@ describe('plugin integration', function () {
     it('has an incoming paychan', async function () {
       await connectPlugins(this.peerPlugin, this.plugin)
 
-      const paychanid = this.pluginState.incomingPaymentChannelId
+      const paychanid = this.plugin._incomingChannel
       const chan = await this.api.getPaymentChannel(paychanid)
 
       assert.strictEqual(chan.account, this.peer.address)
-      assert.strictEqual(chan.amount, dropsToXrp(COMMON_OPTS.maxAmount))
+      assert.strictEqual(chan.amount, dropsToXrp(COMMON_OPTS.channelAmount))
       assert.strictEqual(chan.balance, '0')
       assert.strictEqual(chan.destination, this.newWallet.address)
       assert.strictEqual(chan.settleDelay, COMMON_OPTS.settleDelay)
 
-      const pubKeyUIntA = this.peerPlugin._paychanContext.state.keyPair.publicKey
-      const expectedPubKey = 'ED' + Buffer.from(pubKeyUIntA)
+      const expectedPubKey = 'ED' + Buffer.from(this.peerPlugin._keyPair.publicKey)
         .toString('hex').toUpperCase()
       assert.strictEqual(chan.publicKey, expectedPubKey)
     })
@@ -202,79 +183,80 @@ describe('plugin integration', function () {
         accounts: [ this.newWallet.address, this.peer.address ]
       })
 
-      this.fulfillment = crypto.randomBytes(32)
+      this.fulfillment = Buffer.from('E4840A1A3C50A1635CC53F637721114BEBAF3EAB02FE1AC7C97A6F100311A3ED', 'hex')
       this.transfer = {
-        id: uuid(),
-        ledger: this.plugin.getInfo().prefix,
-        from: this.plugin.getAccount(),
-        to: this.plugin.getPeerAccount(),
-        expiresAt: new Date(Date.now() + 10000).toISOString(),
-        amount: '5',
-        custom: {
-          field: 'some stuff'
-        },
-        executionCondition: base64url(crypto
-          .createHash('sha256')
-          .update(this.fulfillment)
-          .digest())
+        amount: '10',
+        expiresAt: new Date(Date.now() + 10000),
+        executionCondition: crypto.createHash('sha256').update(this.fulfillment).digest(),
+        destination: 'peer.example',
+        data: Buffer.from('hello world')
       }
 
-      this.autoFulfill = new Promise((resolve, reject) => {
-        this.peerPlugin.on('incoming_prepare', async (transfer) => {
-          await this.peerPlugin.fulfillCondition(
-            transfer.id,
-            base64url(this.fulfillment)
-          )
-          resolve()
+      // this.transfer = {
+      //   id: uuid(),
+      //   ledger: this.plugin.getInfo().prefix,
+      //   from: this.plugin.getAccount(),
+      //   to: this.plugin.getPeerAccount(),
+      //   expiresAt: new Date(Date.now() + 10000).toISOString(),
+      //   amount: '5',
+      //   custom: {
+      //     field: 'some stuff'
+      //   },
+      // executionCondition: base64url(crypto
+      //   .createHash('sha256')
+      //   .update(this.fulfillment)
+      //   .digest())
+      // }
+
+      // this.autoFulfill = new Promise((resolve, reject) => {
+      //   this.peerPlugin.on('incoming_prepare', async (transfer) => {
+      //     await this.peerPlugin.fulfillCondition(
+      //       transfer.id,
+      //       base64url(this.fulfillment)
+      //     )
+      //     resolve()
+      //   })
+      // })
+      // 
+      this.peerPlugin.registerDataHandler((ilp) => {
+        return ilpPacket.serializeIlpFulfill({
+          fulfillment: this.fulfillment,
+          data: Buffer.from('hello world again')
         })
       })
     })
 
     it('submits a claim on disconnect', async function () {
-      await this.plugin.sendTransfer(this.transfer)
-      await this.autoFulfill
-      const peerBalance = await this.peerPlugin.getBalance()
+      await this.plugin.sendMoney(this.transfer.amount)
 
       // disconnect() makes the plugin submit a claim
       await this.peerPlugin.disconnect()
 
       // assert that the balance on-ledger was adjusted
-      const paychanid = await this.pluginState.outgoingChannel.getMax()
-      const chan = await this.api.getPaymentChannel(paychanid.data)
+      const paychanid = this.plugin._outgoingChannel
+      const chan = await this.api.getPaymentChannel(paychanid)
       assert.strictEqual(chan.balance, dropsToXrp(this.transfer.amount))
-
-      // assert that the plugins' stored balance matches the on-ledger balance
-      assert.strictEqual(peerBalance, this.transfer.amount)
-      assert.equal(await this.plugin.getBalance(), -1 * parseInt(this.transfer.amount))
     })
 
     it('funds a paychan', async function () {
-      const chanId = this.pluginState.outgoingPaymentChannelId
-      this.transfer.amount = COMMON_OPTS.maxUnsecured
-      const expectedFundingThreshold = parseInt(COMMON_OPTS.maxAmount) *
-        parseFloat(COMMON_OPTS.fundThreshold)
-
-      // send transfers until the funding threshold is exceeded
-      while (parseFloat(await this.peerPlugin.getBalance()) <= expectedFundingThreshold) {
-        // assert that the plugin has not yet issued a funding tx
-        const chan = await this.api.getPaymentChannel(chanId)
-        assert(chan.amount, dropsToXrp(COMMON_OPTS.maxAmount))
-
-        this.transfer.id = uuid()
-        await this.plugin.sendTransfer(this.transfer)
-        await new Promise((resolve, reject) => {
-          this.plugin.on('outgoing_fulfill', () => resolve())
-        })
-      }
-
-      const expectedAmount = new BigNumber(dropsToXrp(COMMON_OPTS.maxAmount))
-        .mul(2).toString()
-      await new Promise((resolve, reject) => {
+      const expectedAmount = new BigNumber(dropsToXrp(COMMON_OPTS.channelAmount)).times(2)
+      const ledgerClose = new Promise((resolve, reject) => {
         this.api.connection.on('ledgerClosed', async (ev) => {
-          const chan = await this.api.getPaymentChannel(chanId)
-          if (chan.amount === expectedAmount) resolve()
+          const chan = await this.api.getPaymentChannel(this.plugin._outgoingChannel)
+          assert.equal(chan.amount, expectedAmount, 'Channel does not have expected amount')
+          resolve()
         })
       })
+
+      // assert that the channel has the initial amount
+      const chan = await this.api.getPaymentChannel(this.plugin._outgoingChannel)
+      assert.equal(chan.amount, dropsToXrp(COMMON_OPTS.channelAmount))
+
+      // send a transfer that triggers a funding tx
+      const expectedFundingThreshold = parseInt(COMMON_OPTS.channelAmount) *
+        parseFloat(COMMON_OPTS.fundThreshold) + 1
+      await this.plugin.sendMoney(expectedFundingThreshold)
+      return ledgerClose
     })
   })
 })
