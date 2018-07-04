b6c642718147a5e936df2fd1641177931f9cf323
sharafian <ben@sharafian.me>
2018-07-04 12:50:39 -0700

    feat: lower fund threshold

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const {
 // constants
 const CHANNEL_KEYS = 'ilp-plugin-xrp-paychan-channel-keys'
 const DEFAULT_CHANNEL_AMOUNT_XRP = 1
-const DEFAULT_FUND_THRESHOLD = 0.9
+const DEFAULT_FUND_THRESHOLD = 0.5
 
 class PluginXrpPaychan extends PluginBtp {
   constructor (opts) {
